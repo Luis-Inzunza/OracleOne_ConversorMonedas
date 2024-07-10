1824d89852624f14fab18e345b04b305b64f49db
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2024-07-10 09:47:08 -0600

    Primera Prueba de conexion y Menu
    Se creo un menu como clase para modularizar el codigo y se realizo una conexion y respuesta de la API con exito

--- ConversorMonedas/src/Principal/Main.java
+++ ConversorMonedas/src/Principal/Main.java
@@ -1,7 +1,56 @@
 package Principal;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import Secciones.Menu;
+
 public class Main {
     public static void main(String[] args) {
+        Menu inicio = new Menu();
+        while(true){
+            int respuesta = inicio.Bienvenida();
+            System.out.println(respuesta);
+            String direccion = "https://v6.exchangerate-api.com/v6/e9dc1ede6703f48c3a59acf3/latest/USD";
+            try {
 
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+            /*
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(.UPPER_CAMEL_CASE).create();
+            TituloOMdb miTituloOmdb = gson.fromJson(json, TituloOMdb.class);
+            System.out.println("Titulo: " + miTituloOmdb);
+
+            Titulo miTitulo = new Titulo(miTituloOmdb);
+            System.out.println(miTitulo);
+
+            FileWriter escritura = new FileWriter("peliculas.txt");
+            escritura.write(miTitulo.toString());
+            escritura.close();
+
+             */
+
+            }catch (Exception e){
+                System.out.println("Ocurrio un error");
+                System.out.println(e.getMessage());
+
+            }
+        }
     }
+
 }
---
+++ ConversorMonedas/src/Secciones/Menu.java
@@ -0,0 +1,21 @@
+package Secciones;
+
+import java.util.Scanner;
+
+public class Menu {
+    public int Bienvenida(){
+        System.out.println("Bienvenido al Conversor de monedas Java!");
+        System.out.println("Por favor, selecciona una opcion escribiendo el numero");
+        System.out.println("1) Dolar Estadounidense => Pesos Mexicanos");
+        System.out.println("2) Pesos Mexicanos => Dolar Estadounidense");
+        System.out.println("3) Dolar Estadounidense => Dolar Australiano");
+        System.out.println("4) Dolar Australiano => Dolar Estadounidense");
+        System.out.println("5) Dolar Estadounidense => Dolar Canadiense");
+        System.out.println("6) Dolar Canadiense => Dolar Estadounidense");
+        System.out.println("7) Salir");
+        Scanner lectura = new Scanner(System.in);
+        var busqueda = lectura.nextLine();
+
+        return Integer.parseInt(busqueda);
+    }
+}
